870e502d7e8ef2c676b9a1bdd1fbd7a77e183be3
Ольга <olyaborodikhina@yandex.ru>
2017-06-05 23:01:29 +0600

    Нажатие на кнопку “+”: Нажатие на кнопку “Log out”:

--- app/src/main/java/com/example/exam/NewActivity.java
+++ app/src/main/java/com/example/exam/NewActivity.java
@@ -1,9 +1,11 @@
 package com.example.exam;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
 /**
  * Created by Ольга on 20.05.2017.
@@ -18,7 +20,31 @@ public class NewActivity extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.m_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logOut://добавить проверку-  Если все поля заполнены
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                return true;
+
+            case R.id.Plus:
+                Intent intentAdd = new Intent(this, AddActivity.class);
+                startActivity(intentAdd);
+                return true;
+
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
