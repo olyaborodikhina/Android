cca1b93fca03fbdb2b8de66d4c31345f50bbf80b
Ольга <olyaborodikhina@yandex.ru>
2017-06-06 00:00:39 +0600

    Экранн ADD Shapes - готов, и частично обработка кнопки ADD

--- app/src/main/java/com/example/exam/AddActivity.java
+++ app/src/main/java/com/example/exam/AddActivity.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSpinner;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Ольга on 03.06.2017.
@@ -14,22 +19,66 @@ import android.widget.Button;
 
 public class AddActivity extends AppCompatActivity implements View.OnClickListener {
 
-   private Button add;
+    private Button add;
+    private AppCompatSpinner spinnerRectangel,spinnerColor;
+
+    private EditText titleText,descriptionText;
+    private TextView chooseShape,chooseColor,addTitle,addDescription;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_activity);
 
+        spinnerRectangel = (AppCompatSpinner) findViewById(R.id.Rectangel_spinner);
+        String[] data = new String[]{"Rectangle", "Circle", "Line"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, data);
+        spinnerRectangel.setAdapter(adapter);
+        spinnerRectangel.setSelection(0);
+
+        spinnerColor = (AppCompatSpinner) findViewById(R.id.color_spinner);
+        String[] dataColor = new String[]{"Red", "Green", "Blue"};
+        ArrayAdapter<String> adapterColor = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, dataColor);
+        spinnerColor.setAdapter(adapterColor);
+        spinnerColor.setSelection(0);
+
         add = (Button) findViewById(R.id.add);
         add.setOnClickListener(this);
+
+        titleText = (EditText) findViewById(R.id.Title_text);
+        descriptionText = (EditText) findViewById(R.id.Description_text);
+        chooseShape = (TextView) findViewById(R.id.Choose_shape);
+        chooseColor = (TextView) findViewById(R.id.Choose_color);
+        addTitle = (TextView) findViewById(R.id.add_title);
+        addDescription = (TextView) findViewById(R.id.add_Description);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.add://добавить проверку-  Если все поля заполнены
-                Intent intent = new Intent(this, NewActivity.class);
+            case R.id.add:
+                String titleTextStr = titleText.getText().toString();
+                String descriptionStr = descriptionText.getText().toString();
+                String chooseShapeStr = chooseShape.getText().toString();
+                String  chooseColorStr = chooseColor.getText().toString();
+                String addTitleStr = addTitle.getText().toString();
+                String addDescriptionStr = addDescription.getText().toString();
+
+                //Если хотя бы одно из полей не заполнено
+                if((titleTextStr.length() == 0) || (descriptionStr.length() == 0) ||
+                        (chooseColorStr.length() == 0) || (chooseShapeStr.length() == 0) ||
+                        (addTitleStr.length() == 0) || (addDescriptionStr.length() == 0)){
+//                if((addTitleStr.length() == 0) || (addDescriptionStr.length() == 0)){
+                    Toast toast = Toast.makeText(getApplicationContext(), "Please fill in all fields", Toast.LENGTH_SHORT);
+                    toast.show();
+                }else {
+                    Intent intent = new Intent(this, NewActivity.class);
+                    startActivity(intent);
+                }
                 break;
+
         }
     }
 }
